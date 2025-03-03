9dbc1ff401261fc8de77e5d167122b4e0cdd706e
rambedade <rambedade2308@gmail.com>
2025-03-03 20:26:35 +0530

    changed api/booking

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key";
 
